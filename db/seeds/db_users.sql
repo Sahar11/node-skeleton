258b07f4f8968cf57158c2efe740438daabd2f18
Sahar <saharhalim151@gmail.com>
2022-01-10 17:43:29 +0000

    fix css

--- db/seeds/db_users.sql
+++ db/seeds/db_users.sql
@@ -7,7 +7,7 @@
 -- INSERT INTO orders (users_id, total_price) VALUES (2, 20);
 
 
-INSERT INTO menus (item, description, thumbnail_url, price) VALUES ('pizza','large pepperoni', 'https://www.budgetbytes.com/wp-content/uploads/2010/07/Classic-Homemade-Pizza-Dough-close.jpg', 10);
+INSERT INTO menus (item, description, thumbnail_url, price) VALUES ('pizza','large pepperoni', 'https://www.healthylittlefoodies.com/wp-content/uploads/2019/10/Tortilla-Pizza-500x500.jpg', 10);
 INSERT INTO menus (item, description, thumbnail_url, price) VALUES ('pizza','large tomatina', 'https://cdn.pixabay.com/photo/2020/04/29/03/30/pizza-5107039_1280.jpg', 20);
 INSERT INTO menus (item, description, thumbnail_url, price) VALUES ('pizza','large margherita', 'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F06%2F15%2Fmozzarella-pizza-margherita-FT-RECIPE0621.jpg', 15);
 -- INSERT INTO restaurants (name, menu_id, order_id) VALUES ('RoJoSas Pizza', 1, 1);
