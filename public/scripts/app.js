51030d658d5921fd2ae32a93f74c733c0009cdd1
Sahar <saharhalim151@gmail.com>
2022-01-10 21:18:14 +0000

    add cart counter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
 // Client facing scripts here
+// add products to cart
+const cart_count = require('./cart_count');
+
+function addToCartClicked (id) {
+  const addToCart = document.getElementsByClassName('add-to-cart');
+
+console.log(id);
+  for (var i = 0; i < addToCart.length; i++) {
+    button = addToCart[i];
+  }
+}
+
---
+++ public/scripts/cart_count.js
@@ -0,0 +1,7 @@
+let itemCount = 0;
+
+    $(".add-to-cart").click(function () {
+      itemCount++;
+      $(".cart-quantity").html(itemCount);
+    });
+
